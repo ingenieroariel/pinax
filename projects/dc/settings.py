ca3c8472f23cc7d8b2acd833407f5f82779a0d22
pydanny <pydanny@45601e1e-1555-4799-bd40-45c8c71eef50>
2009-01-22 18:21:00 +0000

    [dc] change pinax name for demo

--- projects/dc/settings.py
+++ projects/dc/settings.py
@@ -180,7 +180,7 @@ NOTIFICATION_LANGUAGE_MODULE = 'account.Account'
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
 CONTACT_EMAIL = "feedback@example.com"
-SITE_NAME = "Pinax"
+SITE_NAME = "Spacebook"
 LOGIN_URL = "/account/login"
 LOGIN_REDIRECT_URLNAME = "what_next"
 
